1b74fd5af2436bcbcdd3155c4e474aa98aead426
Luis Q. Haitzer <luis@haitzer.de>
2023-01-01 09:21:01 +0100

    Adding eslint & eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort', 'import', 'prettier'],
+  ignorePatterns: ['**/*.css', '**/*.scss'],
+  rules: {
+    'prettier/prettier': ['warn'],
+    'no-console': [
+      'warn',
+      {
+        allow: ['info', 'error'],
+      },
+    ],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
