d9c5151bb7926b2d963e59711864ca17fc060170
Leopold Tüting <leopold@tueting.net>
2023-01-08 17:42:57 +0100

    changes to turbo repo

--- packages/core/src/Scene/untils/resolveScenePositions.ts
+++ packages/core/src/Scene/untils/resolveScenePositions.ts
@@ -2,5 +2,7 @@ import type { ResolvedShape } from '../../types/ResolvedShape';
 import type { Scene } from '../Scene';
 
 export const resolveScenePositions = (scene: Scene): ResolvedShape[] => {
+  // is only there to prevent build from crashing to to typecheck
+  console.log(scene);
   return [];
 };
